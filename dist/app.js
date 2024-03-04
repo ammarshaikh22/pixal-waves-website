79dd74538829eb44c0771bcce5fa4cc97702e003
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2024-03-04 14:43:04 +0500

    n

--- dist/app.js
+++ dist/app.js
@@ -6,12 +6,20 @@ mobile header section start
 const menu = document.querySelector('.menu');
 const menu_icon = document.querySelector('.menu-bar');
 const cross_icon = document.querySelector('.cross-bar');
+const all_links = document.querySelectorAll('.links');
 //add event on element
 menu_icon.addEventListener('click', () => {
     menu.classList.add('d-menu');
     cross_icon.classList.add('d-crossbar');
+    menu_icon.classList.add('h-menu_icon');
 });
 cross_icon.addEventListener('click', () => {
+    menu_icon.classList.remove('h-menu_icon');
     menu.classList.remove('d-menu');
     cross_icon.classList.remove('d-crossbar ');
 });
+all_links.forEach((elem) => elem.addEventListener('click', () => {
+    menu_icon.classList.remove('h-menu_icon');
+    menu.classList.remove('d-menu');
+    menu_icon.addEventListener('click', (e) => e.preventDefault());
+}));
